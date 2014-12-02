69b4480505acfc82a50515c2dbd63290338152ee
mubarak1361 <disfrutarz@gmail.com>
2014-12-02 19:15:49 +0530

    bug fixed

--- src/com/example/croutontoast/MainActivity.java
+++ src/com/example/croutontoast/MainActivity.java
@@ -32,8 +32,8 @@ public class MainActivity extends FragmentActivity implements OnClickListener {
         errorToast = new CroutonToast("Error Message" , Color.parseColor("#e51c23"));
         
 		success = (Button)findViewById(R.id.success);
-		warning = (Button) findViewById(R.id.warning);
-		error = (Button)  findViewById(R.id.error);
+		warning = (Button)findViewById(R.id.warning);
+		error = (Button)findViewById(R.id.error);
 			
 		success.setOnClickListener(this);
 		warning.setOnClickListener(this);
@@ -45,8 +45,8 @@ public class MainActivity extends FragmentActivity implements OnClickListener {
 	public void onClick(View v) {
 		
 		switch (v.getId()) {
+		
 		case R.id.success:
-			
 			ft = getSupportFragmentManager().beginTransaction();
 	        ft.setCustomAnimations(R.animator.enter,R.animator.exit);
 	        ft.replace(R.id.crouton, successToast);
@@ -130,4 +130,54 @@ public class MainActivity extends FragmentActivity implements OnClickListener {
 		 handler.removeCallbacksAndMessages(null);
 		 
 	}
+	
+	
+	
+	
+	/*public int getImageId(int groupPosition, int childPosition, Resources res) {
+
+		TypedArray intarray = res.obtainTypedArray(R.array.all_imgs);
+		int len1 = intarray.length();
+		int[] resIds1 = new int[len1];
+
+		for (int i = 0; i < len1; i++) {
+			resIds1[i] = intarray.getResourceId(i, 0);
+			Log.d("GroupArratIDs",
+					"Res Id " + i + " is " + Integer.toHexString(resIds1[i]));
+		}
+
+		TypedArray intarray1 = res.obtainTypedArray(resIds1[groupPosition]);
+		int len = intarray1.length();
+		int[] resIds2 = new int[len];
+		for (int k = 0; k < len; k++) {
+			resIds2[k] = intarray1.getResourceId(k, 0);
+			Log.v("ChildArrayIDs",
+					"Res Id " + k + " is " + Integer.toHexString(resIds2[k]));
+		}
+		return resIds2[childPosition];
+
+	}
+
+	public ArrayList<String> getString(int position, Resources res) {
+
+		TypedArray sringarray = res.obtainTypedArray(R.array.all_imgs);
+		ArrayList<ArrayList<String>> title = new ArrayList<ArrayList<String>>();
+
+		int n = sringarray.length();
+		for (int i = 0; i < n; ++i) {
+			int id = sringarray.getResourceId(i, 0);
+			if (id > 0) {
+				ArrayList<String> temp_name = new ArrayList<String>(
+						Arrays.asList(res.getStringArray(id)));
+				title.add(temp_name);
+			} else {
+
+			}
+		}
+
+		sringarray.recycle();
+
+		return title.get(position);
+	}
+*/
 }
